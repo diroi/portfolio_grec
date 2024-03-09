7752a663c2bda9f9a14c358e12923b9a18d6d2a0
diroi <121102664+diroi@users.noreply.github.com>
2024-03-09 18:07:49 +0000

    new file:   accueil/OICCIP.png new file:   accueil/accueil.css new file:   accueil/accueil.js new file:   accueil/banksy.jpg new file:   cookies.js new file:   index.html

---
+++ accueil/accueil.js
@@ -0,0 +1,5 @@
+function createCookie() {
+  const date = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires="+ d.toUTCString();
+  }
---
+++ cookies.js
@@ -0,0 +1,41 @@
+window.addEventListener("load", (event) => {
+    if (!getCookie("date") || !getCookie("loc") || !getCookie("ip")) {
+      createCookie();
+    }
+  });
+  
+  function createCookie() {
+    const date = new Date();
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const loc = JSON.stringify(position.coords);
+          const ip = fetch("https://checkip.amazonaws.com/")
+            .then((res) => res.text())
+            .then((ip) => ip.trim());
+  
+          document.cookie = `date=${encodeURIComponent(date)}; loc=${encodeURIComponent(loc)}; ip=${encodeURIComponent(ip)}; path=/`;
+        },
+        (error) => {
+          console.log(`Error getting location: ${error.message}`);
+          const loc = "Error: " + error.message;
+          document.cookie = `date=${encodeURIComponent(date)}; loc=${encodeURIComponent(loc)}; ip=${encodeURIComponent("")}; path=/`;
+        }
+      );
+    } else {
+      const loc = "Geolocation not supported";
+      document.cookie = `date=${encodeURIComponent(date)}; loc=${encodeURIComponent(loc)}; ip=${encodeURIComponent("")}; path=/`;
+    }
+  }
+  
+  function getCookie(cookieName) {
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      if (cookie.startsWith(cookieName + '=')) {
+        const cookieValue = cookie.substring(cookieName.length + 1);
+        return decodeURIComponent(cookieValue);
+      }
+    }
+    return null;
+  }
